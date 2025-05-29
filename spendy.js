8cbe41bc72697703a0cb8164563b6239ba1c8fa5
bhavy1204 <bhavysarwa51@gmail.com>
2025-05-29 09:48:21 +0530

    feat: made all entries choices interactive thorugh inquerior

--- spendy.js
+++ spendy.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
 import inquirer from 'inquirer';
+import { validate } from 'uuid';
 
 let total = 0;
 
-let entry = [];
+let allEntries = [];
 
 const addExpense = async () => {
     if (total <= 0) {
@@ -18,19 +19,19 @@ const addExpense = async () => {
         name: "amount",
         message: "Enter your expense : ",
     }]);
-    if ( amountRes.amount <= 0) {
+    if (amountRes.amount <= 0) {
         console.error("Not valid amount");
-        await new Promise(r => setTimeout(r, 2000));
+        await new Promise(r => setTimeout(r, 1000));
         console.clear();
     } else {
         let createdAt = new Date().toLocaleString();
         let noteRes = await inquirer.prompt([{
-            message:"Add note : ",
-            name:"note",
-            type:"input"
+            message: "Add note : ",
+            name: "note",
+            type: "input"
         }])
-        total -=  amountRes.amount;
-        entry.push({amount:amountRes.amount, note: noteRes.note,createdAt:createdAt}); 
+        total -= amountRes.amount;
+        allEntries.push({ amount: amountRes.amount, note: noteRes.note, createdAt: createdAt });
         console.log("Entry added successfully");
         await new Promise(r => setTimeout(r, 1000));
         console.clear();
@@ -50,15 +51,45 @@ const addIncome = async () => {
     } else {
         let createdAt = new Date().toLocaleString();
         let noteRes = await inquirer.prompt([{
-            message:"Add note : ",
-            name:"note",
-            type:"input"
+            message: "Add note : ",
+            name: "note",
+            type: "input"
         }])
         total += amountRes.amount;
-        entry.push({amount:amountRes.amount, note: noteRes.note,createdAt:createdAt}); 
+        allEntries.push({ amount: amountRes.amount, note: noteRes.note, createdAt: createdAt });
         console.log("Entry added successfully");
         await new Promise(r => setTimeout(r, 1000));
         console.clear();
+    }
+}
+
+const entryDetails = async (idx) => {
+    console.log("YOU ARE IN entryMenu");
+    console.log(allEntries[idx]);
+}
+
+const showEntries = async () => {
+    if (allEntries.length === 0) {
+        console.log("No entries to show");
+        return;
+    } else {
+        const choices = allEntries.map((e, i) => ({
+            name: `Entry no:${i + 1}\nCreated at: ${e.createdAt}\nAmount: ${e.amount}\nNote: ${e.note}`,
+            value: i,
+        }));
+
+        choices.push({name: "EXIT", value: -1});
+
+        const res = await inquirer.prompt([{
+            name: "entryChoice",
+            message: "ALL ENTRIES",
+            type: "list",
+            choices: choices
+        }]);
+
+        if(res.entryChoice===-1)
+            return;
+        await entryDetails(res.entryChoice);
     }
 }
 
@@ -83,9 +114,10 @@ const menu = async () => {
         } else if (res.choice === "2. income") {
             await addIncome();
         } else if (res.choice === "3. view total") {
-            entry.forEach(e => {
-                console.log(`${e.createdAt}\nAmount: ${e.amount}\nNotes: ${e.note}\n-----------------------------`);
-            });
+            if (allEntries.length === 0)
+                console.log("No entriess to show");
+            else
+                await showEntries();
         } else if (res.choice === "4. Exit") {
             exit = true;
         }
